0496c3b436baa6fa62070f193072692a1d65305f
Betzabeth Narvaez <betzynarv@gmail.com>
2021-09-13 13:14:05 +0900

    initial commit

---
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.helloandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.icon);
+        Toast.makeText(this, "안녕하세요, 버튼 클릭입나다", Toast.LENGTH_LONG).show();
+
+    }
+}
