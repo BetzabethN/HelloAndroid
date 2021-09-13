fa7feced1b36f8b3bcf69e7c9c24bdd12f0b5e6f
Betzabeth Narvaez <betzynarv@gmail.com>
2021-09-13 13:26:08 +0900

    fixed button

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.helloandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
