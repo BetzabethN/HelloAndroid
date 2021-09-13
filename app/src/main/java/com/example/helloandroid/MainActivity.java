a2700e911d67512b44547408e027c53cca96db73
Betzabeth Narvaez <betzynarv@gmail.com>
2021-09-13 14:00:45 +0900

    on click image edit

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void onClick(View view) {
         ImageView imageView = findViewById(R.id.imageView);
         imageView.setImageResource(R.drawable.icon);
-        Toast.makeText(this, "안녕하세요, 버튼 클릭입나다", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "이미지를 보여줍니다", Toast.LENGTH_LONG).show();
 
     }
 }
